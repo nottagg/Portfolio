ef32ef336d1d3806f41ec0ddd5e3d541c4cd28ec
jamescongdon <jamescongdon@Mac.attlocal.net>
2025-03-28 21:10:31 -0400

    Create vite, add hello world path with new go

--- backend/main.go
+++ backend/main.go
@@ -2,15 +2,13 @@ package main
 
 import (
 	"fmt"
-	"log"
-	"net/http"
 )
 
 // https://dev.to/moficodes/build-your-first-rest-api-with-go-2gcj
 
 func main() {
-    http.HandleFunc("/all", handleAll)
-	fmt.Print("Listening on port 8080")
-    log.Fatal(http.ListenAndServe(":8080", nil))
+	server := APIConstructor(":8080")
+	fmt.Printf("Starting server on port " + server.addr)
+	RunServer(server)
 
 }
